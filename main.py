dec78c7a5383c6a7adf1e1fb2e26e5ff15316430
Tomas Martinez Anderson <60526206+Tomtron623@users.noreply.github.com>
2024-04-10 10:27:00 -0400

    Encoder Function

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("this is a test")
-print("danny")
+key = {"1":"4", "2":"5", "3":"6", "4":"7", "5": "8", "6":"9", "7":"1", "8":"2", "9":"3"}
+def encoder(value):
+    newValue = ""
+    for i in range(len(value)):
+        newValue += key.get(value[i])
+    return newValue
